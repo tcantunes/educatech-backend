7aca307779062d1bb9645568e538cc8a971e88f4
tcantunes <103459242+tcantunes@users.noreply.github.com>
2024-09-18 16:58:34 -0300

    feat: Iniciando projeto

---
+++ controllers/authController.js
@@ -0,0 +1,63 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+};
+
+exports.register = async (req, res) => {
+  const { name, city, state, email, password, confirmPassword } = req.body;
+
+  try {
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: 'Senhas não coincidem' });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'Usuário já existe' });
+    }
+
+    const user = await User.create({
+      name,
+      city,
+      state,
+      email,
+      password,
+    });
+
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      return res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      return res.status(401).json({ message: 'Credenciais inválidas' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
+};
